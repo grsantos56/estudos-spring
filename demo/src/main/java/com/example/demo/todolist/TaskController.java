83f8593c60a2610e807121b19063273f17916560
Gabriel Rdorigues dos Santos <gabrielrsantos999@gmail.com>
2025-06-06 06:27:57 -0300

    verificando as datas

--- demo/src/main/java/com/example/demo/todolist/TaskController.java
+++ demo/src/main/java/com/example/demo/todolist/TaskController.java
@@ -2,11 +2,13 @@ package com.example.demo.todolist;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -17,12 +19,19 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping("/create_task")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         System.out.println("chegou no controller" + request.getAttribute("userId"));
 
         var userId = request.getAttribute("userId");
         taskModel.setUserId((UUID) userId);
 
-        return this.taskRepository.save(taskModel);
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(400).body("Start date must be in the future");
+        }if(taskModel.getStartAt().isAfter(taskModel.getStartAt()) || taskModel.getEndAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(400).body("End date must be after start date");
+        }
+
+        return ResponseEntity.status(200).body(taskModel);
     }
 }
