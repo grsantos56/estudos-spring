55a05a01516da1b15a69e52be2fe605598363084
Gabriel Rdorigues dos Santos <gabrielrsantos999@gmail.com>
2025-06-06 20:14:20 -0300

    try catch no titulo

--- demo/src/main/java/com/example/demo/todolist/TaskModel.java
+++ demo/src/main/java/com/example/demo/todolist/TaskModel.java
@@ -29,5 +29,12 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    public void setTitle(String title) throws Exception{
+        if (title != null && title.length() > 50) {
+            throw new Exception("Title must be 50 characters or less");
+        }
+        this.title = title;
+    }
+
 
 }
